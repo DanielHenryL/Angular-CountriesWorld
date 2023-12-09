7440b43e557320c872e07b730f9d248d4814d403
DanielHenryL <lagunasdaniel001@gmail.com>
2023-12-09 11:32:48 -0500

    enlazando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home/components/home-page/home-page.component';
+import { AboutPageComponent } from './home/components/about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component: HomePageComponent
+  },
+  {
+    path:'about',
+    component:AboutPageComponent
+  },
+  {
+    path:'countries',
+    loadChildren: ()=> import('./countries/countries.module').then(m => m.CountriesModule)
+  },
+  {
+    path:'**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
